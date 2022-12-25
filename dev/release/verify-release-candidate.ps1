a5bb8238eb647d333f20dccfc502a70f6f382edb
David Li <li.davidm96@gmail.com>
2022-12-25 11:12:12 -0500

    chore(dev/release): implement RC verification for Windows
    Fixes #278.

--- dev/release/verify-release-candidate.ps1
+++ dev/release/verify-release-candidate.ps1
@@ -31,10 +31,11 @@ if ($env:VERBOSE) {
 }
 
 if ($args.Count -eq 0) {
-    $version = "HEAD"
+    $Version = "HEAD"
     $SourceKind = "local"
 } elseif ($args.Count -eq 2) {
-    $version = $args[0]
+    $Version = $args[0]
+    $RcNumber = $args[1]
     $SourceKind = "tarball"
 } else {
     $script = Split-Path $PSCommandPath -leaf
@@ -73,7 +74,35 @@ Show-Header "Ensure Source Directory"
 if ($SourceKind -eq "local") {
     $ArrowSourceDir = Join-Path $PSScriptRoot "..\.." | Resolve-Path | % { $_.Path }
 } else {
-    exit 1
+    $ArrowDistUrl = "https://dist.apache.org/repos/dist/dev/arrow"
+    $DistName = "apache-arrow-adbc-$($Version)"
+    $DistUrl = "$($ArrowDistUrl)/$($DistName)-rc$($RcNumber)/$DistName.tar.gz"
+    $Sha256Url = "$($ArrowDistUrl)/$($DistName)-rc$($RcNumber)/$DistName.tar.gz.sha256"
+    $Sha512Url = "$($ArrowDistUrl)/$($DistName)-rc$($RcNumber)/$DistName.tar.gz.sha512"
+    $DistPath = Join-Path $ArrowTempDir "$($DistName).tar.gz"
+    $Sha256Path = Join-Path $ArrowTempDir "$($DistName).tar.gz.sha256"
+    $Sha512Path = Join-Path $ArrowTempDir "$($DistName).tar.gz.sha512"
+    $ArrowSourceDir = Join-Path $ArrowTempDir $DistName
+
+    echo "Fetching $($DistUrl)"
+    New-Item -ItemType Directory -Path $ArrowSourceDir -Force
+    Invoke-WebRequest -Uri $DistUrl -OutFile $DistPath
+    Invoke-WebRequest -Uri $Sha256Url -OutFile $Sha256Path
+    Invoke-WebRequest -Uri $Sha512Url -OutFile $Sha512Path
+
+    $ExpectedSha256 = (Get-Content $Sha256Path).Split(" ")[0]
+    $ExpectedSha512 = (Get-Content $Sha512Path).Split(" ")[0]
+
+    if (-not ((Get-FileHash -Algorithm SHA256 $DistPath).Hash -eq $ExpectedSha256)) {
+        echo "SHA256 hash mismatch"
+        exit 1
+    }
+    if (-not ((Get-FileHash -Algorithm SHA512 $DistPath).Hash -eq $ExpectedSha512)) {
+        echo "SHA512 hash mismatch"
+        exit 1
+    }
+
+    tar -C $ArrowSourceDir --strip-components 1 -xf $DistPath
 }
 
 echo "Using $($ArrowSourceDir)"
