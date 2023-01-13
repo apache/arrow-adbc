f602952029619cc08e6a9a056b4a55bf84420c5b
Matt Topol <zotthewizard@gmail.com>
2023-01-13 12:53:08 -0500

    fix line length

--- go/adbc/pkg/flightsql/utils.c
+++ go/adbc/pkg/flightsql/utils.c
@@ -70,7 +70,8 @@ AdbcStatusCode AdbcConnectionGetInfo(struct AdbcConnection* connection,
                                      uint32_t* info_codes, size_t info_codes_length,
                                      struct ArrowArrayStream* out,
                                      struct AdbcError* error) {
-  return FlightSQLConnectionGetInfo(connection, info_codes, info_codes_length, out, error);
+  return FlightSQLConnectionGetInfo(connection, info_codes, info_codes_length, out,
+                                    error);
 }
 
 AdbcStatusCode AdbcConnectionGetObjects(struct AdbcConnection* connection, int depth,
@@ -80,7 +81,7 @@ AdbcStatusCode AdbcConnectionGetObjects(struct AdbcConnection* connection, int d
                                         struct ArrowArrayStream* out,
                                         struct AdbcError* error) {
   return FlightSQLConnectionGetObjects(connection, depth, catalog, db_schema, table_name,
-                                    table_type, column_name, out, error);
+                                       table_type, column_name, out, error);
 }
 
 AdbcStatusCode AdbcConnectionGetTableSchema(struct AdbcConnection* connection,
@@ -89,7 +90,7 @@ AdbcStatusCode AdbcConnectionGetTableSchema(struct AdbcConnection* connection,
                                             struct ArrowSchema* schema,
                                             struct AdbcError* error) {
   return FlightSQLConnectionGetTableSchema(connection, catalog, db_schema, table_name,
-                                        schema, error);
+                                           schema, error);
 }
 
 AdbcStatusCode AdbcConnectionGetTableTypes(struct AdbcConnection* connection,
@@ -104,7 +105,7 @@ AdbcStatusCode AdbcConnectionReadPartition(struct AdbcConnection* connection,
                                            struct ArrowArrayStream* out,
                                            struct AdbcError* error) {
   return FlightSQLConnectionReadPartition(connection, serialized_partition,
-                                       serialized_length, out, error);
+                                          serialized_length, out, error);
 }
 
 AdbcStatusCode AdbcConnectionCommit(struct AdbcConnection* connection,
@@ -180,7 +181,7 @@ AdbcStatusCode AdbcStatementExecutePartitions(struct AdbcStatement* statement,
                                               int64_t* rows_affected,
                                               struct AdbcError* error) {
   return FlightSQLStatementExecutePartitions(statement, schema, partitions, rows_affected,
-                                          error);
+                                             error);
 }
 
 ADBC_EXPORT
