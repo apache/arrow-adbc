9ccd99880aa1a4cf9890a5d5982739ea9f356946
Dewey Dunnington <dewey@fishandwhistle.net>
2023-02-15 16:08:40 -0400

    some RAT

--- r/adbcsqlite/src/init.c
+++ r/adbcsqlite/src/init.c
@@ -1,7 +1,3 @@
-#define R_NO_REMAP
-#include <R.h>
-#include <Rinternals.h>
-
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
