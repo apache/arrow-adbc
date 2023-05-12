965b97fd6b5dbf57aa058f9df15e2b79bd6c3300
Will Ayd <william.ayd@icloud.com>
2023-05-12 15:10:46 -0700

    Working tests with random_access_iterator

--- c/driver/postgresql/connection.cc
+++ c/driver/postgresql/connection.cc
@@ -37,7 +37,7 @@ static const uint32_t kSupportedInfoCodes[] = {
 };
 
 struct PqRecord {
-  const char* db_name;
+  const char* data;
   const int len;
   const bool is_null;
 };
@@ -69,19 +69,33 @@ class PqResultRow {
       ++(*this);
       return retval;
     }
+
+    iterator operator+=(const int& n) {
+      iter_col_num_ += n;
+      return *this;
+    }
+
+    iterator operator+(const int& n) {
+      iterator tmp(*this);
+      tmp += n;
+      return tmp;
+    }
+
+    PqRecord operator[](const int& n) { return *(*this + n); }
+
     bool operator==(iterator other) const {
       return iter_result_ == other.iter_result_ && iter_row_num_ == other.iter_row_num_ &&
              iter_col_num_ == other.iter_col_num_;
     }
     bool operator!=(iterator other) const { return !(*this == other); }
     PqRecord operator*() {
-      const char* db_name = PQgetvalue(iter_result_, iter_row_num_, iter_col_num_);
+      const char* data = PQgetvalue(iter_result_, iter_row_num_, iter_col_num_);
       const int len = PQgetlength(iter_result_, iter_row_num_, iter_col_num_);
       const bool is_null = PQgetisnull(iter_result_, iter_row_num_, iter_col_num_);
 
-      return PqRecord{db_name, len, is_null};
+      return PqRecord{data, len, is_null};
     }
-    using iterator_category = std::forward_iterator_tag;
+    using iterator_category = std::random_access_iterator_tag;
     using difference_type = std::ptrdiff_t;
     using value_type = PqRecord;
     using pointer = PqRecord*;
@@ -101,19 +115,16 @@ class PqResultHelper {
  public:
   PqResultHelper(PGconn* conn, const char* query) : conn_(conn) {
     query_ = std::string(query);
-  }
-  pg_result* Execute() {
     result_ = PQexec(conn_, query_.c_str());
-    ExecStatusType pq_status = PQresultStatus(result_);
-    if (pq_status == PGRES_TUPLES_OK) {
-      num_rows_ = PQntuples(result_);
-    }
-    return result_;
   }
 
-  ~PqResultHelper() {
-    if (result_ != nullptr) PQclear(result_);
-  }
+  ExecStatusType Status() { return PQresultStatus(result_); }
+
+  ~PqResultHelper() { PQclear(result_); }
+
+  int NumRows() { return PQntuples(result_); }
+
+  int NumColumns() { return PQnfields(result_); }
 
   class iterator {
     pg_result* iter_result_;
@@ -121,8 +132,8 @@ class PqResultHelper {
     int iter_curr_row_ = 0;
 
    public:
-    explicit iterator(pg_result* result, int num_rows, int curr_row = 0)
-        : iter_result_(result), iter_num_rows_(num_rows), iter_curr_row_(curr_row) {}
+    explicit iterator(pg_result* result, int curr_row = 0)
+        : iter_result_(result), iter_curr_row_(curr_row) {}
     iterator& operator++() {
       iter_curr_row_++;
       return *this;
@@ -133,9 +144,7 @@ class PqResultHelper {
       return retval;
     }
     bool operator==(iterator other) const {
-      return iter_result_ == other.iter_result_ &&
-             iter_num_rows_ == other.iter_num_rows_ &&
-             iter_curr_row_ == other.iter_curr_row_;
+      return iter_result_ == other.iter_result_ && iter_curr_row_ == other.iter_curr_row_;
     }
     bool operator!=(iterator other) const { return !(*this == other); }
     PqResultRow operator*() { return PqResultRow(iter_result_, iter_curr_row_); }
@@ -146,14 +155,13 @@ class PqResultHelper {
     using reference = const PqResultRow::iterator&;
   };
 
-  iterator begin() { return iterator(result_, num_rows_, 0); }
-  iterator end() { return iterator(result_, num_rows_, num_rows_); }
+  iterator begin() { return iterator(result_, 0); }
+  iterator end() { return iterator(result_, NumRows()); }
 
  private:
   pg_result* result_ = nullptr;
   PGconn* conn_;
   std::string query_;
-  int num_rows_ = 0;
 };
 
 }  // namespace
@@ -272,10 +280,9 @@ AdbcStatusCode PostgresConnectionGetObjectsImpl(
     PqResultHelper result_helper = PqResultHelper{conn, query.buffer};
     StringBuilderReset(&query);
 
-    result_helper.Execute();
     for (auto row : result_helper) {
       for (auto pq_record : row) {
-        auto db_name = pq_record.db_name;
+        auto db_name = pq_record.data;
         CHECK_NA(INTERNAL,
                  ArrowArrayAppendString(catalog_name_col, ArrowCharView(db_name)), error);
         if (depth == ADBC_OBJECT_DEPTH_CATALOGS) {
@@ -357,39 +364,40 @@ AdbcStatusCode PostgresConnection::GetTableSchema(const char* catalog,
 
   PqResultHelper result_helper = PqResultHelper{conn_, query.buffer};
   StringBuilderReset(&query);
-  pg_result* result = result_helper.Execute();
 
-  ExecStatusType pq_status = PQresultStatus(result);
-  auto uschema = nanoarrow::UniqueSchema();
-
-  if (pq_status == PGRES_TUPLES_OK) {
-    int num_rows = PQntuples(result);
+  if (result_helper.Status() != PGRES_TUPLES_OK) {
+    SetError(error, "%s%s", "Failed to get table schema: ", PQerrorMessage(conn_));
+    final_status = ADBC_STATUS_IO;
+  } else {
+    auto uschema = nanoarrow::UniqueSchema();
     ArrowSchemaInit(uschema.get());
-    CHECK_NA(INTERNAL, ArrowSchemaSetTypeStruct(uschema.get(), num_rows), error);
+    CHECK_NA(INTERNAL, ArrowSchemaSetTypeStruct(uschema.get(), result_helper.NumRows()),
+             error);
 
     ArrowError na_error;
-    for (int row = 0; row < num_rows; row++) {
-      const char* colname = PQgetvalue(result, row, 0);
+    int row_counter = 0;
+    for (auto row : result_helper) {
+      auto iter = row.begin();
+      const char* colname = iter[0].data;
       const Oid pg_oid = static_cast<uint32_t>(
-          std::strtol(PQgetvalue(result, row, 1), /*str_end=*/nullptr, /*base=*/10));
+          std::strtol(iter[1].data, /*str_end=*/nullptr, /*base=*/10));
 
       PostgresType pg_type;
       if (type_resolver_->Find(pg_oid, &pg_type, &na_error) != NANOARROW_OK) {
-        SetError(error, "%s%d%s%s%s%" PRIu32, "Column #", row + 1, " (\"", colname,
-                 "\") has unknown type code ", pg_oid);
+        SetError(error, "%s%d%s%s%s%" PRIu32, "Column #", row_counter + 1, " (\"",
+                 colname, "\") has unknown type code ", pg_oid);
         final_status = ADBC_STATUS_NOT_IMPLEMENTED;
-        break;
+        goto loopExit;
       }
-
-      CHECK_NA(INTERNAL, pg_type.WithFieldName(colname).SetSchema(uschema->children[row]),
+      CHECK_NA(INTERNAL,
+               pg_type.WithFieldName(colname).SetSchema(uschema->children[row_counter]),
                error);
+      row_counter++;
     }
-  } else {
-    SetError(error, "%s%s", "Failed to get table schema: ", PQerrorMessage(conn_));
-    final_status = ADBC_STATUS_IO;
+    uschema.move(schema);
   }
+loopExit:
 
-  uschema.move(schema);
   return final_status;
 }
 
