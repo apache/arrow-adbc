77f0c929092cee5dc23b8ee2e2d16c48215a5ea8
David Li <li.davidm96@gmail.com>
2023-05-17 13:54:35 -0400

    ci: re-enable Snowflake driver on aarch64
    Fixes #675.

--- ci/scripts/python_wheel_unix_build.sh
+++ ci/scripts/python_wheel_unix_build.sh
@@ -82,22 +82,13 @@ build_drivers "${source_dir}" "${build_dir}"
 check_visibility $ADBC_FLIGHTSQL_LIBRARY
 check_visibility $ADBC_POSTGRESQL_LIBRARY
 check_visibility $ADBC_SQLITE_LIBRARY
-if [[ -f "$ADBC_SNOWFLAKE_LIBRARY" ]]; then
-    check_visibility $ADBC_SNOWFLAKE_LIBRARY
-else
-    echo "adbc_driver_snowflake not built"
-fi
+check_visibility $ADBC_SNOWFLAKE_LIBRARY
 
 # https://github.com/pypa/pip/issues/7555
 # Get the latest pip so we have in-tree-build by default
 python -m pip install --upgrade pip auditwheel cibuildwheel delocate setuptools wheel
 
 for component in $COMPONENTS; do
-    if [[ "$component" = "adbc_driver_snowflake" ]] && [[ "${VCPKG_ARCH}" = "arm64" ]]; then
-        echo "Skipping $component on arm64"
-        continue
-    fi
-
     pushd ${source_dir}/python/$component
 
     component_dashes=${component//_/-}
