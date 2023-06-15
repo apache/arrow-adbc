7e3e08b917dde8dad24e917624ee6e4a4a02eb3c
William Ayd <william.ayd@icloud.com>
2023-06-15 16:29:54 -0700

    feat(c/driver/postgresql): Support float type (#807)

--- c/driver/postgresql/postgresql_test.cc
+++ c/driver/postgresql/postgresql_test.cc
@@ -469,8 +469,6 @@ class PostgresStatementTest : public ::testing::Test,
   void TestSqlIngestUInt16() { GTEST_SKIP() << "Not implemented"; }
   void TestSqlIngestUInt32() { GTEST_SKIP() << "Not implemented"; }
   void TestSqlIngestUInt64() { GTEST_SKIP() << "Not implemented"; }
-  void TestSqlIngestFloat32() { GTEST_SKIP() << "Not implemented"; }
-  void TestSqlIngestFloat64() { GTEST_SKIP() << "Not implemented"; }
   void TestSqlIngestBinary() { GTEST_SKIP() << "Not implemented"; }
 
   void TestSqlPrepareErrorParamCountMismatch() { GTEST_SKIP() << "Not yet implemented"; }
