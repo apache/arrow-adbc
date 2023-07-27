fe3646340a0d17844214c8990d60efa9ad4a4ee3
David Li <li.davidm96@gmail.com>
2023-07-27 11:02:37 -0400

    feat(python): expose ADBC 1.1.0 features (#937)

--- python/adbc_driver_postgresql/tests/test_dbapi.py
+++ python/adbc_driver_postgresql/tests/test_dbapi.py
@@ -17,6 +17,7 @@
 
 from typing import Generator
 
+import pyarrow
 import pytest
 
 from adbc_driver_postgresql import StatementOptions, dbapi
@@ -26,6 +27,32 @@ from adbc_driver_postgresql import StatementOptions, dbapi
 def postgres(postgres_uri: str) -> Generator[dbapi.Connection, None, None]:
     with dbapi.connect(postgres_uri) as conn:
         yield conn
+
+
+def test_conn_current_catalog(postgres: dbapi.Connection) -> None:
+    assert postgres.adbc_current_catalog != ""
+
+
+def test_conn_current_db_schema(postgres: dbapi.Connection) -> None:
+    assert postgres.adbc_current_db_schema == "public"
+
+
+def test_conn_change_db_schema(postgres: dbapi.Connection) -> None:
+    assert postgres.adbc_current_db_schema == "public"
+
+    with postgres.cursor() as cur:
+        cur.execute("CREATE SCHEMA IF NOT EXISTS dbapischema")
+
+    assert postgres.adbc_current_db_schema == "public"
+    postgres.adbc_current_db_schema = "dbapischema"
+    assert postgres.adbc_current_db_schema == "dbapischema"
+
+
+def test_conn_get_info(postgres: dbapi.Connection) -> None:
+    info = postgres.adbc_get_info()
+    assert info["driver_name"] == "ADBC PostgreSQL Driver"
+    assert info["driver_adbc_version"] == 1_001_000
+    assert info["vendor_name"] == "PostgreSQL"
 
 
 def test_query_batch_size(postgres: dbapi.Connection):
@@ -47,6 +74,12 @@ def test_query_batch_size(postgres: dbapi.Connection):
         cur.adbc_statement.set_options(
             **{StatementOptions.BATCH_SIZE_HINT_BYTES.value: "1"}
         )
+        assert (
+            cur.adbc_statement.get_option_int(
+                StatementOptions.BATCH_SIZE_HINT_BYTES.value
+            )
+            == 1
+        )
         cur.execute("SELECT * FROM test_batch_size")
         table = cur.fetch_arrow_table()
         assert len(table.to_batches()) == 65536
@@ -54,15 +87,53 @@ def test_query_batch_size(postgres: dbapi.Connection):
         cur.adbc_statement.set_options(
             **{StatementOptions.BATCH_SIZE_HINT_BYTES.value: "4096"}
         )
+        assert (
+            cur.adbc_statement.get_option_int(
+                StatementOptions.BATCH_SIZE_HINT_BYTES.value
+            )
+            == 4096
+        )
         cur.execute("SELECT * FROM test_batch_size")
         table = cur.fetch_arrow_table()
         assert 64 <= len(table.to_batches()) <= 256
+
+
+def test_query_cancel(postgres: dbapi.Connection) -> None:
+    with postgres.cursor() as cur:
+        cur.execute("DROP TABLE IF EXISTS test_batch_size")
+        cur.execute("CREATE TABLE test_batch_size (ints INT)")
+        cur.execute(
+            """
+            INSERT INTO test_batch_size (ints)
+            SELECT generated :: INT
+            FROM GENERATE_SERIES(1, 65536) temp(generated)
+        """
+        )
+
+        cur.execute("SELECT * FROM test_batch_size")
+        cur.adbc_cancel()
+        # XXX(https://github.com/apache/arrow-adbc/issues/940):
+        # PyArrow swallows the errno and doesn't set it into the
+        # OSError, so we have no clue what happened here. (Though the
+        # driver does properly return ECANCELED.)
+        with pytest.raises(OSError, match="canceling statement"):
+            cur.fetchone()
+
+
+def test_query_execute_schema(postgres: dbapi.Connection) -> None:
+    with postgres.cursor() as cur:
+        schema = cur.adbc_execute_schema("SELECT 1 AS foo")
+        assert schema == pyarrow.schema([("foo", "int32")])
 
 
 def test_query_trivial(postgres: dbapi.Connection):
     with postgres.cursor() as cur:
         cur.execute("SELECT 1")
         assert cur.fetchone() == (1,)
+
+
+def test_stmt_ingest(postgres: dbapi.Connection) -> None:
+    pass
 
 
 def test_ddl(postgres: dbapi.Connection):
