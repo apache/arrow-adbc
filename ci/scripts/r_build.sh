500cb6b3692a76abf325c8da1227e15a0c5c73fc
Dewey Dunnington <dewey@voltrondata.com>
2023-08-15 18:12:47 -0300

    docs: Generate and host R documentation (#977)
    This PR adds the standard-issue pkgdown site build to the documentation
    (and fixes a few build errors that were identified as a result of
    building in that environment). The Packaging / Documentation job
    contains an artifact with the generated documentation and I've included
    a few images below.
    
    <img width="270" alt="Screenshot 2023-08-15 at 4 55 35 PM"
    src="https://github.com/apache/arrow-adbc/assets/10995762/3841ee1b-e344-4683-ab9a-02c60be0f4ca">
    
    <img width="604" alt="Screen Shot 2023-06-28 at 3 14 32 PM"
    src="https://github.com/apache/arrow-adbc/assets/10995762/f6e27b08-f52c-4ece-aab8-b6afde1623df">
    
    <img width="1110" alt="Screenshot 2023-08-15 at 4 56 06 PM"
    src="https://github.com/apache/arrow-adbc/assets/10995762/8824e453-8c24-4867-91e8-5148ed3c7e50">
    
    Closes #875.

---
+++ ci/scripts/r_build.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+set -e
+
+: ${BUILD_ALL:=1}
+: ${BUILD_DRIVER_FLIGHTSQL:=${BUILD_ALL}}
+: ${BUILD_DRIVER_MANAGER:=${BUILD_ALL}}
+: ${BUILD_DRIVER_POSTGRESQL:=${BUILD_ALL}}
+: ${BUILD_DRIVER_SQLITE:=${BUILD_ALL}}
+: ${BUILD_DRIVER_SNOWFLAKE:=${BUILD_ALL}}
+
+if [[ $(uname) = "Darwin" ]]; then
+    ADBC_LIBRARY_SUFFIX="dylib"
+else
+    ADBC_LIBRARY_SUFFIX="so"
+fi
+
+install_pkg() {
+    local -r source_dir="${1}"
+    local -r install_dir="${2}"
+    local -r pkg="${3}"
+    R CMD INSTALL "${source_dir}/r/${pkg}" --preclean --library="${install_dir}"
+}
+
+main() {
+    local -r source_dir="${1}"
+    local -r install_dir="${2}"
+
+    R_LIBS_USER="${install_dir}" R -e 'install.packages("nanoarrow", repos = "https://cloud.r-project.org/")' --vanilla
+
+    if [[ "${BUILD_DRIVER_MANAGER}" -gt 0 ]]; then
+        install_pkg "${source_dir}" "${install_dir}" adbcdrivermanager
+    fi
+
+    if [[ "${BUILD_DRIVER_FLIGHTSQL}" -gt 0 ]]; then
+        install_pkg "${source_dir}" "${install_dir}" adbcflightsql
+    fi
+
+    if [[ "${BUILD_DRIVER_POSTGRESQL}" -gt 0 ]]; then
+        install_pkg "${source_dir}" "${install_dir}" adbcpostgresql
+    fi
+
+    if [[ "${BUILD_DRIVER_SQLITE}" -gt 0 ]]; then
+        install_pkg "${source_dir}" "${install_dir}" adbcsqlite
+    fi
+
+    if [[ "${BUILD_DRIVER_SNOWFLAKE}" -gt 0 ]]; then
+        install_pkg "${source_dir}" "${install_dir}" adbcsnowflake
+    fi
+}
+
+main "$@"
