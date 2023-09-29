58c2e65211972bf29b972aa015c3e35147c047e3
Dewey Dunnington <dewey@fishandwhistle.net>
2023-09-29 15:55:44 -0300

    fix

--- r/adbcdrivermanager/src/radbc.cc
+++ r/adbcdrivermanager/src/radbc.cc
@@ -139,6 +139,7 @@ extern "C" SEXP RAdbcDatabaseNew(SEXP driver_init_func_xptr) {
 extern "C" SEXP RAdbcMoveDatabase(SEXP database_xptr) {
   AdbcDatabase* database = adbc_from_xptr<AdbcDatabase>(database_xptr);
   SEXP database_xptr_new = PROTECT(adbc_allocate_xptr<AdbcDatabase>());
+  R_RegisterCFinalizer(database_xptr_new, &finalize_database_xptr);
   AdbcDatabase* database_new = adbc_from_xptr<AdbcDatabase>(database_xptr_new);
 
   memcpy(database_new, database, sizeof(AdbcDatabase));
@@ -208,6 +209,7 @@ extern "C" SEXP RAdbcConnectionNew(void) {
 extern "C" SEXP RAdbcMoveConnection(SEXP connection_xptr) {
   AdbcConnection* connection = adbc_from_xptr<AdbcConnection>(connection_xptr);
   SEXP connection_xptr_new = PROTECT(adbc_allocate_xptr<AdbcConnection>());
+  R_RegisterCFinalizer(connection_xptr_new, &finalize_connection_xptr);
   AdbcConnection* connection_new = adbc_from_xptr<AdbcConnection>(connection_xptr_new);
 
   memcpy(connection_new, connection, sizeof(AdbcConnection));
@@ -396,6 +398,7 @@ extern "C" SEXP RAdbcStatementNew(SEXP connection_xptr) {
 extern "C" SEXP RAdbcMoveStatement(SEXP statement_xptr) {
   AdbcStatement* statement = adbc_from_xptr<AdbcStatement>(statement_xptr);
   SEXP statement_xptr_new = PROTECT(adbc_allocate_xptr<AdbcStatement>());
+  R_RegisterCFinalizer(statement_xptr_new, &finalize_statement_xptr);
   AdbcStatement* statement_new = adbc_from_xptr<AdbcStatement>(statement_xptr_new);
 
   memcpy(statement_new, statement, sizeof(AdbcStatement));
