cd6a2c4603d2df93a82a53c36bdef47f148c68f3
David Coe <coedavid@umich.edu>
2023-10-20 11:09:44 -0400

    line endings

--- csharp/test/Drivers/Snowflake/ClientTests.cs
+++ csharp/test/Drivers/Snowflake/ClientTests.cs
@@ -132,11 +132,6 @@ namespace Apache.Arrow.Adbc.Tests.Drivers.Interop.Snowflake
                         while (reader.Read())
                         {
                             count++;
-
-                            for(int i=0;i< reader.FieldCount;i++)
-                            {
-                                Console.WriteLine($"{reader.GetName(i)}: {reader.GetValue(i)}");
-                            }
                         }
                     }
                     finally { reader.Close(); }
