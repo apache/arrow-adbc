879ff324aac9f03c0e93145d87b92be27c5692bb
Ruoxuan Wang <52271048+ruowan@users.noreply.github.com>
2023-12-05 21:02:39 +0800

    test(csharp/test/Drivers/Snowflake): refactor snowflake test framework (#1323)

--- csharp/test/Drivers/Snowflake/SnowflakeTestingUtils.cs
+++ csharp/test/Drivers/Snowflake/SnowflakeTestingUtils.cs
@@ -34,6 +34,7 @@ namespace Apache.Arrow.Adbc.Tests.Drivers.Interop.Snowflake
         public const string PASSWORD = "password";
         public const string WAREHOUSE = "adbc.snowflake.sql.warehouse";
         public const string AUTH_TYPE = "adbc.snowflake.sql.auth_type";
+        public const string AUTH_TOKEN = "adbc.snowflake.sql.client_option.auth_token";
         public const string HOST = "adbc.snowflake.sql.uri.host";
         public const string PKCS8_VALUE = "adbc.snowflake.sql.client_option.jwt_private_key_pkcs8_value";
         public const string PKCS8_PASS = "adbc.snowflake.sql.client_option.jwt_private_key_pkcs8_password";
@@ -78,11 +79,17 @@ namespace Apache.Arrow.Adbc.Tests.Drivers.Interop.Snowflake
                 { SnowflakeParameters.USERNAME, testConfiguration.User },
                 { SnowflakeParameters.PASSWORD, testConfiguration.Password },
                 { SnowflakeParameters.WAREHOUSE, testConfiguration.Warehouse },
-                { SnowflakeParameters.AUTH_TYPE, testConfiguration.AuthenticationType },
                 { SnowflakeParameters.USE_HIGH_PRECISION, testConfiguration.UseHighPrecision.ToString().ToLowerInvariant() }
             };
 
-            if (!string.IsNullOrWhiteSpace(testConfiguration.Host))
+            if(testConfiguration.Authentication.Default is not null)
+            {
+                parameters[SnowflakeParameters.AUTH_TYPE] = SnowflakeAuthentication.AuthSnowflake;
+                parameters[SnowflakeParameters.USERNAME] = testConfiguration.Authentication.Default.User;
+                parameters[SnowflakeParameters.PASSWORD] = testConfiguration.Authentication.Default.Password;
+            }
+
+            if(!string.IsNullOrWhiteSpace(testConfiguration.Host))
             {
                 parameters[SnowflakeParameters.HOST] = testConfiguration.Host;
             }
