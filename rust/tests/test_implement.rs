1ff35aa14e66878dea309596054dcf26dfc0c334
alexandreyc <alexandre.crayssac@gmail.com>
2024-02-21 17:59:19 +0100

    Add missing call to DatabaseInit

--- rust/tests/test_implement.rs
+++ rust/tests/test_implement.rs
@@ -377,7 +377,13 @@ macro_rules! set_driver_method {
 
 fn get_connection() -> (DriverConnection, Arc<Mutex<PatchableDriver>>) {
     let (builder, mock_driver) = get_database_builder();
-    let conn = builder.init().new_connection().unwrap().init().unwrap();
+    let conn = builder
+        .init()
+        .unwrap()
+        .new_connection()
+        .unwrap()
+        .init()
+        .unwrap();
     (conn, mock_driver)
 }
 
@@ -396,7 +402,7 @@ fn test_database_set_option() {
     );
 
     let builder = builder.set_option("test_key", "test value 😬").unwrap();
-    let database = builder.init();
+    let database = builder.init().unwrap();
     database.set_option("test_key", "test value 😬").unwrap();
 
     set_driver_method!(mock_driver, database_set_option, |_: &str, _: &str| {
@@ -411,7 +417,7 @@ fn test_database_set_option() {
 #[test]
 fn test_connection_set_option() {
     let (builder, mock_driver) = get_database_builder();
-    let conn_builder = builder.init().new_connection().unwrap();
+    let conn_builder = builder.init().unwrap().new_connection().unwrap();
 
     set_driver_method!(
         mock_driver,
