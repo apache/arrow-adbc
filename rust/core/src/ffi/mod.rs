bbbc0da31cddceb75f62b25d99db1e75325f78f3
alexandreyc <alexandre.crayssac@gmail.com>
2024-04-22 10:08:19 +0200

    Add FFI type and method definitions

--- rust/core/src/ffi/mod.rs
+++ rust/core/src/ffi/mod.rs
@@ -16,11 +16,22 @@
 // under the License.
 
 pub mod constants;
+pub(crate) mod methods;
 pub(crate) mod types;
+pub use types::{
+    FFI_AdbcConnection, FFI_AdbcDatabase, FFI_AdbcDriver, FFI_AdbcDriverInitFunc, FFI_AdbcError,
+    FFI_AdbcErrorDetail, FFI_AdbcPartitions, FFI_AdbcStatement, FFI_AdbcStatusCode,
+};
 
-// TODO(alexandreyc): uncomment these lines during follow-up PRs
-// pub(crate) mod methods;
-// pub use types::{
-//     FFI_AdbcConnection, FFI_AdbcDatabase, FFI_AdbcDriver, FFI_AdbcDriverInitFunc, FFI_AdbcError,
-//     FFI_AdbcErrorDetail, FFI_AdbcPartitions, FFI_AdbcStatement, FFI_AdbcStatusCode,
-// };
+use crate::error::{Error, Result};
+
+pub(crate) fn check_status(status: FFI_AdbcStatusCode, error: FFI_AdbcError) -> Result<()> {
+    match status {
+        constants::ADBC_STATUS_OK => Ok(()),
+        _ => {
+            let mut error: Error = error.try_into()?;
+            error.status = status.try_into()?;
+            Err(error)
+        }
+    }
+}
