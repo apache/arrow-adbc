3ee81ce246e7328f16fe158a9def29437912fe2e
Dewey Dunnington <dewey@fishandwhistle.net>
2024-05-28 21:28:25 -0300

    maybe fix macro

--- adbc.h
+++ adbc.h
@@ -152,7 +152,7 @@ struct ArrowArrayStream {
 // Storage class macros for Windows
 // Allow overriding/aliasing with application-defined macros
 #if !defined(ADBC_EXPORT)
-#if defined(_WIN32)
+#if defined(_MSC_VER)
 #if defined(ADBC_EXPORTING)
 #define ADBC_EXPORT __declspec(dllexport)
 #else
@@ -160,7 +160,7 @@ struct ArrowArrayStream {
 #endif  // defined(ADBC_EXPORTING)
 #else
 #define ADBC_EXPORT
-#endif  // defined(_WIN32)
+#endif  // defined(_MSC_VER)
 #endif  // !defined(ADBC_EXPORT)
 
 /// \defgroup adbc-error-handling Error Handling
