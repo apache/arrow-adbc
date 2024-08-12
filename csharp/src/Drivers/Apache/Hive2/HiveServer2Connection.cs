e549e4a09020e5a9373f4b25263c1384cdd64ba7
Bruce Irschick <bruce.irschick@improving.com>
2024-08-12 15:55:50 -0700

    remove unnecessary using

--- csharp/src/Drivers/Apache/Hive2/HiveServer2Connection.cs
+++ csharp/src/Drivers/Apache/Hive2/HiveServer2Connection.cs
@@ -17,11 +17,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Net.Http.Headers;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Apache.Arrow.Adbc.Drivers.Apache.Spark;
 using Apache.Arrow.Ipc;
 using Apache.Hive.Service.Rpc.Thrift;
 using Thrift.Protocol;
