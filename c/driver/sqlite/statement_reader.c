3b1387fa723d20ad1f30af5dc03e679d3a879e20
Dewey Dunnington <dewey@fishandwhistle.net>
2024-10-08 14:32:29 -0500

    maybe add to sqlite

--- c/driver/sqlite/statement_reader.c
+++ c/driver/sqlite/statement_reader.c
@@ -89,8 +89,11 @@ AdbcStatusCode AdbcSqliteBinderSet(struct AdbcSqliteBinder* binder,
       switch (value_view.type) {
         case NANOARROW_TYPE_STRING:
         case NANOARROW_TYPE_LARGE_STRING:
+        case NANOARROW_TYPE_STRING_VIEW:
         case NANOARROW_TYPE_BINARY:
         case NANOARROW_TYPE_LARGE_BINARY:
+        case NANOARROW_TYPE_FIXED_SIZE_BINARY:
+        case NANOARROW_TYPE_BINARY_VIEW:
           break;
         default:
           SetError(error, "Column %d dictionary has unsupported type %s", i,
@@ -326,7 +329,9 @@ AdbcStatusCode AdbcSqliteBinderBindNext(struct AdbcSqliteBinder* binder, sqlite3
     } else {
       switch (binder->types[col]) {
         case NANOARROW_TYPE_BINARY:
-        case NANOARROW_TYPE_LARGE_BINARY: {
+        case NANOARROW_TYPE_LARGE_BINARY:
+        case NANOARROW_TYPE_FIXED_SIZE_BINARY:
+        case NANOARROW_TYPE_BINARY_VIEW: {
           struct ArrowBufferView value =
               ArrowArrayViewGetBytesUnsafe(binder->batch.children[col], binder->next_row);
           status = sqlite3_bind_blob(stmt, col + 1, value.data.as_char, value.size_bytes,
@@ -359,6 +364,7 @@ AdbcStatusCode AdbcSqliteBinderBindNext(struct AdbcSqliteBinder* binder, sqlite3
           status = sqlite3_bind_int64(stmt, col + 1, value);
           break;
         }
+        case NANOARROW_TYPE_HALF_FLOAT:
         case NANOARROW_TYPE_FLOAT:
         case NANOARROW_TYPE_DOUBLE: {
           double value = ArrowArrayViewGetDoubleUnsafe(binder->batch.children[col],
@@ -367,7 +373,8 @@ AdbcStatusCode AdbcSqliteBinderBindNext(struct AdbcSqliteBinder* binder, sqlite3
           break;
         }
         case NANOARROW_TYPE_STRING:
-        case NANOARROW_TYPE_LARGE_STRING: {
+        case NANOARROW_TYPE_LARGE_STRING:
+        case NANOARROW_TYPE_STRING_VIEW: {
           struct ArrowBufferView value =
               ArrowArrayViewGetBytesUnsafe(binder->batch.children[col], binder->next_row);
           status = sqlite3_bind_text(stmt, col + 1, value.data.as_char, value.size_bytes,
