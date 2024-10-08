c74f3d32c3d2448288eb18eb8bdac720883ec18f
Matthijs Brobbel <m1brobbel@gmail.com>
2024-10-08 04:10:48 +0200

    chore(rust)!: use `arrow-*` subcrates and bump to `53.1.0` (#2208)
    As discussed in
    https://github.com/apache/arrow-adbc/pull/1725#discussion_r1567512784,
    we can now use the `arrow-*` subcrates. This also bumps these `arrow`
    crates to the latest release. This is a breaking change for the
    `adbc_core` crate.

--- rust/core/src/ffi/methods.rs
+++ rust/core/src/ffi/methods.rs
@@ -19,8 +19,8 @@
 
 use std::os::raw::{c_char, c_int};
 
-use arrow::ffi::{FFI_ArrowArray, FFI_ArrowSchema};
-use arrow::ffi_stream::FFI_ArrowArrayStream;
+use arrow_array::ffi::{FFI_ArrowArray, FFI_ArrowSchema};
+use arrow_array::ffi_stream::FFI_ArrowArrayStream;
 
 use super::{
     constants::ADBC_STATUS_NOT_IMPLEMENTED, FFI_AdbcConnection, FFI_AdbcDatabase, FFI_AdbcError,
