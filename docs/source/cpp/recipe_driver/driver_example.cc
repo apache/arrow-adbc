bb4664cfff24eccfd3dbc2549e83933203c10892
David Li <li.davidm96@gmail.com>
2024-10-29 05:34:47 -0400

    Fix indent

--- docs/source/cpp/recipe_driver/driver_example.cc
+++ docs/source/cpp/recipe_driver/driver_example.cc
@@ -87,21 +87,18 @@
 /// to reduce the verbosity of the implementation:
 ///
 /// * :cpp:class:`adbc::driver::Option` : Options can be set on an ADBC database,
-/// connection, and
-///   statmenent. They can be strings, opaque binary, doubles, or integers.
-///   The ``Option`` class abstracts the details of how to get, set, and parse
-///   these values.
+///   connection, and statmenent. They can be strings, opaque binary, doubles, or
+///   integers.  The ``Option`` class abstracts the details of how to get, set,
+///   and parse these values.
 /// * :cpp:class:`adbc::driver::Status`: The ``Status`` is the ADBC driver
-/// framework's error handling
-///   mechanism: functions with no return value that can fail return a ``Status``.
-///   You can use ``UNWRAP_STATUS(some_call())`` as shorthand for
-///   ``Status status = some_call(); if (!status.ok()) return status;`` to
-///   succinctly propagate errors.
+///   framework's error handling mechanism: functions with no return value that
+///   can fail return a ``Status``.  You can use ``UNWRAP_STATUS(some_call())`` as
+///   shorthand for ``Status status = some_call(); if (!status.ok()) return
+///   status;`` to succinctly propagate errors.
 /// * :cpp:class:`adbc::driver::Result`: The ``Result<T>`` is used as a return
-/// value for functions that
-///   on success return a value of type ``T`` and on failure communicate their
-///   error using a ``Status``. You can use ``UNWRAP_RESULT(some_type value,
-///   some_call())`` as shorthand for
+///   value for functions that on success return a value of type ``T`` and on
+///   failure communicate their error using a ``Status``. You can use
+///   ``UNWRAP_RESULT(some_type value, some_call())`` as shorthand for
 ///
 ///   .. code-block:: cpp
 ///
