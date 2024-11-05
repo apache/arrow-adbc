28b8c1b73d0e925deb3e21e8c238f32252279933
Dewey Dunnington <dewey@voltrondata.com>
2024-11-05 18:16:08 +0000

    feat(c/driver/postgresql): Enable basic connect/query workflow for Redshift (#2219)
    Just following up on #1563 to see if the missing `typarray` column is
    the only issue. To get the details right might be a large project, but
    we might be able to support a basic connection without too much effort.
    Paramter binding and non-COPY result fetching seem to work...the default
    query fetch method (COPY) is not supported, `connection_get_info()`
    fails, and at a glance, `connection_get_objects()` might be returning
    incorrect results (and fails at the column depth).
    
    ``` r
    library(adbcdrivermanager)
    
    db <- adbc_database_init(
      adbcpostgresql::adbcpostgresql(),
      uri = Sys.getenv("ADBC_REDSHIFT_TEST_URI"),
      adbc.postgresql.load_array_types = FALSE
    )
    
    con <- db |> 
      adbc_connection_init()
    
    stmt <- con |> 
      adbc_statement_init(adbc.postgresql.use_copy = FALSE)
    
    stream <- nanoarrow::nanoarrow_allocate_array_stream()
    stmt |> 
      adbc_statement_bind(data.frame(45)) |> 
      adbc_statement_set_sql_query("SELECT 1 + $1 as foofy, 'string' as foofy_string") |> 
      adbc_statement_execute_query(stream)
    #> [1] -1
    
    tibble::as_tibble(stream)
    #> # A tibble: 1 × 2
    #>   foofy foofy_string
    #>   <dbl> <chr>       
    #> 1    46 string
    ```
    
    <sup>Created on 2024-10-04 with [reprex
    v2.1.1](https://reprex.tidyverse.org)</sup>
    
    ---------
    
    Co-authored-by: William Ayd <william.ayd@icloud.com>

--- c/driver/postgresql/result_reader.cc
+++ c/driver/postgresql/result_reader.cc
@@ -174,10 +174,10 @@ Status PqResultArrayReader::Initialize(int64_t* rows_affected) {
 
   for (int i = 0; i < helper_.NumColumns(); i++) {
     PostgresType child_type;
-    UNWRAP_NANOARROW(na_error_, Internal,
-                     type_resolver_->Find(helper_.FieldType(i), &child_type, &na_error_));
+    UNWRAP_ERRNO(Internal,
+                 type_resolver_->FindWithDefault(helper_.FieldType(i), &child_type));
 
-    UNWRAP_ERRNO(Internal, child_type.SetSchema(schema_->children[i]));
+    UNWRAP_ERRNO(Internal, child_type.SetSchema(schema_->children[i], vendor_name_));
     UNWRAP_ERRNO(Internal,
                  ArrowSchemaSetName(schema_->children[i], helper_.FieldName(i)));
 
