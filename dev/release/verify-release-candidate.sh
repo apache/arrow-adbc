4b68705a49de251ed3e3c597bc5514915d973c40
David Li <li.davidm96@gmail.com>
2025-01-05 21:45:08 -0500

    Revert "try to get a new pyarrow (2)"
    This reverts commit f6731d8c913ff3f0f593d78aabd3e0b46460758c.

--- dev/release/verify-release-candidate.sh
+++ dev/release/verify-release-candidate.sh
@@ -564,7 +564,7 @@ test_python() {
   # XXX: pin Python for now since various other packages haven't caught up
   # XXX: force a later PyArrow to try to get a recent build
   # https://github.com/conda-forge/abseil-cpp-feedstock/issues/87
-  maybe_setup_conda --file "${ADBC_DIR}/ci/conda_env_python.txt" python=3.12 'pyarrow-all>=18.1.0' || exit 1
+  maybe_setup_conda --file "${ADBC_DIR}/ci/conda_env_python.txt" python=3.12 'pyarrow>=18.1.0' || exit 1
 
   if [ "${USE_CONDA}" -gt 0 ]; then
     CMAKE_PREFIX_PATH="${CONDA_BACKUP_CMAKE_PREFIX_PATH}:${CMAKE_PREFIX_PATH}"
