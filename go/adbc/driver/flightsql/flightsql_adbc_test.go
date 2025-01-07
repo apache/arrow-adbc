196522bb2f11665c7b6e0d1ed98da174315a19d9
David Li <li.davidm96@gmail.com>
2025-01-07 05:11:03 -0500

    ci: make sure bash has pipefail (#2423)
    Continuation of #2421.

--- go/adbc/driver/flightsql/flightsql_adbc_test.go
+++ go/adbc/driver/flightsql/flightsql_adbc_test.go
@@ -1055,6 +1055,14 @@ type DomainSocketTests struct {
 }
 
 func (suite *DomainSocketTests) SetupSuite() {
+	// This doesn't appear to run under Windows
+	// transport: Error while dialing: dial unix
+	// /Users/RUNNER~1/AppData/Local/Temp/adbc-flight-sql-tests-1919020904/adbc.sock:
+	// connect: A socket operation encountered a dead network.
+	if runtime.GOOS == "windows" {
+		suite.T().Skip()
+	}
+
 	suite.alloc = memory.NewCheckedAllocator(memory.DefaultAllocator)
 
 	tempDir, err := os.MkdirTemp("", "adbc-flight-sql-tests-*")
