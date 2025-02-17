3fe032928ad5b5ac0c070b8dd7fa4b1f4a5f62bf
Aman Goyal <amangoyal@microsoft.com>
2025-02-17 22:34:33 +0530

    Addressing comments & added Readme file

--- csharp/src/Drivers/Apache/Spark/SparkHttpConnection.cs
+++ csharp/src/Drivers/Apache/Spark/SparkHttpConnection.cs
@@ -129,7 +129,7 @@ namespace Apache.Arrow.Adbc.Drivers.Apache.Spark
             }
         }
 
-        internal override IArrowArrayStream NewReader<T>(T statement, Schema schema) => new HiveServer2Reader(statement, schema, dataTypeConversion: statement.Connection.DataTypeConversion, true);
+        internal override IArrowArrayStream NewReader<T>(T statement, Schema schema) => new HiveServer2Reader(statement, schema, dataTypeConversion: statement.Connection.DataTypeConversion, enableBatchSizeStopCondition: true);
 
         protected override TTransport CreateTransport()
         {
