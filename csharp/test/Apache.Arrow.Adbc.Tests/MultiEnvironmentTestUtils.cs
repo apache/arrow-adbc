80094cea78a6e78bf394c45a3d3cfae152122b15
davidhcoe <13318837+davidhcoe@users.noreply.github.com>
2025-03-02 22:35:01 -0500

    feat(csharp/src/Drivers/BigQuery): choose the first project ID if not specified (#2541)
    - The call to BigQueryClient.CreateQueryJob fails if a project ID is not
    present (even though the `*detect-project-id*` value is passed). This
    change locates the first project ID in the list of project IDs and uses
    it if no project ID is specified.
    - Adds support for multiple BigQuery test environments. Includes some
    refactoring for of Apache.Arrow.Adbc.Tests.Drivers.Interop.FlightSql to
    move common functionality to a shared library.
    
    ---------
    
    Co-authored-by: David Coe <>

---
+++ csharp/test/Apache.Arrow.Adbc.Tests/MultiEnvironmentTestUtils.cs
@@ -0,0 +1,90 @@
+﻿/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Apache.Arrow.Adbc.Tests
+{
+    public static class MultiEnvironmentTestUtils
+    {
+        public static T LoadMultiEnvironmentTestConfiguration<T>(string environmentVariable)
+        {
+            T? testConfiguration = default(T);
+
+            if (!string.IsNullOrWhiteSpace(environmentVariable))
+            {
+                string? environmentValue = Environment.GetEnvironmentVariable(environmentVariable);
+
+                if (!string.IsNullOrWhiteSpace(environmentValue))
+                {
+                    if (File.Exists(environmentValue))
+                    {
+                        // use a JSON file for the various settings
+                        string json = File.ReadAllText(environmentValue);
+
+                        testConfiguration = JsonSerializer.Deserialize<T>(json)!;
+                    }
+                }
+            }
+
+            if (testConfiguration == null)
+                throw new InvalidOperationException($"Cannot execute test configuration from environment variable `{environmentVariable}`");
+
+            return testConfiguration;
+        }
+
+        public static List<TEnvironment> GetTestEnvironments<TEnvironment>(MultiEnvironmentTestConfiguration<TEnvironment> testConfiguration)
+            where TEnvironment : TestConfiguration
+        {
+            if (testConfiguration == null)
+                throw new ArgumentNullException(nameof(testConfiguration));
+
+            if (testConfiguration.Environments == null || testConfiguration.Environments.Count == 0)
+                throw new InvalidOperationException("There are no environments configured");
+
+            List<TEnvironment> environments = new List<TEnvironment>();
+
+            foreach (string environmentName in GetEnvironmentNames(testConfiguration.TestEnvironmentNames))
+            {
+                if (testConfiguration.Environments.TryGetValue(environmentName, out TEnvironment? testEnvironment))
+                {
+                    if (testEnvironment != null)
+                    {
+                        testEnvironment.Name = environmentName;
+                        environments.Add(testEnvironment);
+                    }
+                }
+            }
+
+            if (environments.Count == 0)
+                throw new InvalidOperationException("Could not find a configured environment to execute the tests");
+
+            return environments;
+        }
+
+        private static List<string> GetEnvironmentNames(List<string> names)
+        {
+            if (names == null)
+                return new List<string>();
+
+            return names;
+        }
+    }
+}
