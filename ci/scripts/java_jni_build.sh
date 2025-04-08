d2d32eddcaae522e536fc53e4c838ae4b634417d
David Li <li.davidm96@gmail.com>
2025-04-08 11:00:32 +0900

    feat(java/driver/jni): add JNI bindings to native driver manager (#2401)
    Fixes #2027.

--- ci/scripts/java_jni_build.sh
+++ ci/scripts/java_jni_build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,21 +16,30 @@
 # specific language governing permissions and limitations
 # under the License.
 
-Cython
-importlib-resources
-# libxml2 broke ABI compatibility
-# https://github.com/conda-forge/arrow-cpp-feedstock/issues/1740
-libxml2 <2.14.0
-# nodejs is required by pyright
-nodejs >=13.0.0
-pandas
-pip
-pyarrow-all
-pyright
-pytest
-setuptools
+set -ex
 
-# For integration testing
-polars
-protobuf
-python-duckdb
+: ${ADBC_CMAKE_ARGS:=""}
+: ${CMAKE_BUILD_TYPE:=Debug}
+
+main() {
+    local -r source_dir=${1}
+    local -r build_dir=${2}
+    local -r install_dir=${3}
+
+    mkdir -p "${build_dir}"
+    pushd "${build_dir}"
+
+    set -x
+    cmake "${source_dir}/java" \
+        ${ADBC_CMAKE_ARGS} \
+        -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+        -DCMAKE_INSTALL_PREFIX="${source_dir}/java/driver/jni/src/main/resources/" \
+        -DCMAKE_PREFIX_PATH="${install_dir}/lib/cmake/"
+    set +x
+
+    cmake --build . --target install -j
+
+    popd
+}
+
+main "$@"
