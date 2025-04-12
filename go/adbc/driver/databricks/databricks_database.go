812c544b1836a86fa9e3b6b72e6f9ed091a9c27e
Felipe Oliveira Carvalho <felipekde@gmail.com>
2025-04-12 14:57:23 -0700

    go: Add the Databricks driver implementation

---
+++ go/adbc/driver/databricks/databricks_database.go
@@ -0,0 +1,143 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package databricks
+
+import (
+	"strconv"
+
+	"github.com/apache/arrow-adbc/go/adbc/driver/internal/driverbase"
+	"github.com/databricks/databricks-sdk-go"
+)
+
+type databaseImpl struct {
+	driverbase.DatabaseImplBase
+
+	config *databricks.Config
+}
+
+func SetOption(config *databricks.Config, key string, value string) error {
+	switch key {
+	case OptionStringAuthType:
+		config.AuthType = value
+	case OptionStringClusterID:
+		config.ClusterID = value
+	case OptionStringWarehouseID:
+		config.WarehouseID = value
+	case OptionStringServerlessComputeID:
+		config.ServerlessComputeID = value
+	case OptionStringMetadataServiceURL:
+		config.MetadataServiceURL = value
+	case OptionStringHost:
+		config.Host = value
+	case OptionStringToken:
+		config.Token = value
+	case OptionStringAccountID:
+		config.AccountID = value
+	case OptionStringUsername:
+		config.Username = value
+	case OptionStringPassword:
+		config.Password = value
+	case OptionStringClientID:
+		config.ClientID = value
+	case OptionStringClientSecret:
+		config.ClientSecret = value
+	case OptionStringConfigFile:
+		config.ConfigFile = value
+	case OptionStringProfile:
+		config.Profile = value
+	case OptionStringGoogleServiceAccount:
+		config.GoogleServiceAccount = value
+	case OptionStringGoogleCredentials:
+		config.GoogleCredentials = value
+	case OptionStringAzureResourceID:
+		config.AzureResourceID = value
+	case OptionStringAzureUseMSI:
+		val, err := strconv.ParseBool(value)
+		if err == nil {
+			config.AzureUseMSI = val
+		} else {
+			return err
+		}
+	case OptionStringAzureClientSecret:
+		config.AzureClientSecret = value
+	case OptionStringAzureClientID:
+		config.AzureClientID = value
+	case OptionStringAzureTenantID:
+		config.AzureTenantID = value
+	case OptionStringActionsIDTokenRequestURL:
+		config.ActionsIDTokenRequestURL = value
+	case OptionStringActionsIDTokenRequestToken:
+		config.ActionsIDTokenRequestToken = value
+	case OptionStringAzureEnvironment:
+		config.AzureEnvironment = value
+	case OptionBoolInsecureSkipVerify:
+		val, err := strconv.ParseBool(value)
+		if err == nil {
+			config.InsecureSkipVerify = val
+		} else {
+			return err
+		}
+	case OptionIntHTTPTimeoutSeconds:
+		val, err := strconv.ParseInt(value, 10, 32)
+		if err == nil {
+			config.HTTPTimeoutSeconds = int(val)
+		} else {
+			return err
+		}
+	case OptionIntDebugTruncateBytes:
+		val, err := strconv.ParseInt(value, 10, 32)
+		if err == nil {
+			config.DebugTruncateBytes = int(val)
+		} else {
+			return err
+		}
+	case OptionBoolDebugHeaders:
+		val, err := strconv.ParseBool(value)
+		if err == nil {
+			config.DebugHeaders = val
+		} else {
+			return err
+		}
+	case OptionIntRateLimitPerSecond:
+		val, err := strconv.ParseInt(value, 10, 32)
+		if err == nil {
+			config.RateLimitPerSecond = int(val)
+		} else {
+			return err
+		}
+	case OptionIntRetryTimeoutSeconds:
+		val, err := strconv.ParseInt(value, 10, 32)
+		if err == nil {
+			config.RetryTimeoutSeconds = int(val)
+		} else {
+			return err
+		}
+	}
+	return nil
+}
+
+func (d *databaseImpl) SetOptions(options map[string]string) error {
+	config := databricks.Config{}
+	for k, v := range options {
+		err := SetOption(&config, k, v)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
