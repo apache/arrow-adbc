cbd76470ef51b4d2fb4dc5c9f9a4f1e8fa1fe3b2
David Li <li.davidm96@gmail.com>
2025-06-30 13:04:56 +0900

    build(python): keep building driver manager on manylinux2014 (#3065)
    Fixes #3064.

--- ci/scripts/python_util.sh
+++ ci/scripts/python_util.sh
@@ -149,6 +149,10 @@ function setup_build_vars {
     fi
     # No PyPy, no Python 3.8
     export CIBW_SKIP="pp* cp38-* ${CIBW_SKIP}"
+    # Make sure our manylinux version doesn't creep up (this only matters for
+    # the driver manager)
+    export CIBW_MANYLINUX_X86_64_IMAGE="manylinux2014"
+    export CIBW_MANYLINUX_AARCH64_IMAGE="manylinux2014"
 }
 
 function test_packages {
