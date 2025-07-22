b59c307db321200806caa9b9b01c4c728fbcb45e
Jacky Hu <jacky.hu@databricks.com>
2025-07-22 15:10:39 -0700

    fix(csharp/src/Drivers/Hive2): Fix unnecessary CloseOperation for Metadata Query

--- csharp/src/Drivers/Apache/Hive2/HiveServer2Statement.cs
+++ csharp/src/Drivers/Apache/Hive2/HiveServer2Statement.cs
@@ -457,6 +457,7 @@ namespace Apache.Arrow.Adbc.Drivers.Apache.Hive2
                 ForeignTableName,
                 cancellationToken);
             OperationHandle = resp.OperationHandle;
+            _directResults = resp.DirectResults;
 
             return await GetQueryResult(resp.DirectResults, cancellationToken);
         }
@@ -474,6 +475,7 @@ namespace Apache.Arrow.Adbc.Drivers.Apache.Hive2
                 TableName,
                 cancellationToken);
             OperationHandle = resp.OperationHandle;
+            _directResults = resp.DirectResults;
 
             return await GetQueryResult(resp.DirectResults, cancellationToken);
         }
@@ -482,6 +484,7 @@ namespace Apache.Arrow.Adbc.Drivers.Apache.Hive2
         {
             TGetCatalogsResp resp = await Connection.GetCatalogsAsync(cancellationToken);
             OperationHandle = resp.OperationHandle;
+            _directResults = resp.DirectResults;
 
             return await GetQueryResult(resp.DirectResults, cancellationToken);
         }
@@ -493,6 +496,7 @@ namespace Apache.Arrow.Adbc.Drivers.Apache.Hive2
                 EscapePatternWildcardsInName(SchemaName),
                 cancellationToken);
             OperationHandle = resp.OperationHandle;
+            _directResults = resp.DirectResults;
 
             return await GetQueryResult(resp.DirectResults, cancellationToken);
         }
@@ -507,6 +511,7 @@ namespace Apache.Arrow.Adbc.Drivers.Apache.Hive2
                 tableTypesList,
                 cancellationToken);
             OperationHandle = resp.OperationHandle;
+            _directResults = resp.DirectResults;
 
             return await GetQueryResult(resp.DirectResults, cancellationToken);
         }
