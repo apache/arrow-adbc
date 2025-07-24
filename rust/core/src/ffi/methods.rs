2cdc7289c5d04171528dd85bb8294e9c5d10035d
eitsupi <ts1s1andn@gmail.com>
2025-07-24 10:37:45 +0000

    refactor(rust/core)!: move the driver_manager feature to the new adbc_driver package

--- rust/core/src/ffi/methods.rs
+++ rust/core/src/ffi/methods.rs
@@ -32,7 +32,7 @@ macro_rules! method {
         #[allow(dead_code)]
         pub(crate) type $type_name = unsafe extern "C" fn($( $arg ),*) -> $return_type;
         #[allow(dead_code)]
-        pub(crate) unsafe extern "C" fn $func_name($(_:$arg),*) -> $return_type {
+        pub unsafe extern "C" fn $func_name($(_:$arg),*) -> $return_type {
             $return_value
         }
     };
---
+++ rust/driver_manager/src/error.rs
@@ -0,0 +1,90 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use std::fmt;
+
+use adbc_core::error::Status;
+
+/// An Driver Manager error.
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum Error {
+    Adbc(adbc_core::error::Error),
+    DynamicLibrary(String),
+    Other(String),
+}
+
+/// Result type wrapping [Error].
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl Error {
+    pub fn with_message_and_status(message: impl Into<String>, status: Status) -> Self {
+        (adbc_core::error::Error {
+            message: message.into(),
+            status,
+            vendor_code: 0,
+            sqlstate: [0; 5],
+            details: None,
+        })
+        .into()
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::Adbc(e) => write!(f, "ADBC error: {e}"),
+            Error::DynamicLibrary(s) => write!(f, "Error with dynamic library: {s}"),
+            Error::Other(s) => write!(f, "Other error: {s}"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<adbc_core::error::Error> for Error {
+    fn from(e: adbc_core::error::Error) -> Self {
+        Error::Adbc(e)
+    }
+}
+
+impl From<libloading::Error> for Error {
+    fn from(e: libloading::Error) -> Self {
+        Error::DynamicLibrary(e.to_string())
+    }
+}
+
+impl From<Error> for adbc_core::error::Error {
+    fn from(e: Error) -> Self {
+        match e {
+            Error::Adbc(e) => e,
+            Error::DynamicLibrary(s) => adbc_core::error::Error {
+                message: s,
+                status: Status::Internal,
+                vendor_code: 0,
+                sqlstate: [0; 5],
+                details: None,
+            },
+            Error::Other(s) => adbc_core::error::Error {
+                message: s,
+                status: Status::Internal,
+                vendor_code: 0,
+                sqlstate: [0; 5],
+                details: None,
+            },
+        }
+    }
+}
