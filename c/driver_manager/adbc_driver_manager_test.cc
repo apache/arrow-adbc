6cc290005a5a2bae03fd622be4a931372b4e1b9c
Bryce Mecum <petridish@gmail.com>
2025-08-09 07:13:53 -0700

    Simply setup code

--- c/driver_manager/adbc_driver_manager_test.cc
+++ c/driver_manager/adbc_driver_manager_test.cc
@@ -414,11 +414,8 @@ class DriverManifest : public ::testing::Test {
          }},
     };
 
-    auto temp_path = std::filesystem::temp_directory_path();
-    temp_path /= "adbc_driver_manager_test";
-
-    std::filesystem::create_directories(temp_path);
-    temp_dir = temp_path;
+    temp_dir = std::filesystem::temp_directory_path() / "adbc_driver_manager_test";
+    std::filesystem::create_directories(temp_dir);
 #endif
   }
 
