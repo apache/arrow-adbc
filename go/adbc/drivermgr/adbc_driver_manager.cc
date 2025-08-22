4152f1f391a4e15b03c8cb01ae641cbe46df0261
Matt Topol <zotthewizard@gmail.com>
2025-08-22 14:27:43 -0400

    maybe this time?

--- go/adbc/drivermgr/adbc_driver_manager.cc
+++ go/adbc/drivermgr/adbc_driver_manager.cc
@@ -273,7 +273,7 @@ AdbcStatusCode LoadDriverManifest(const std::filesystem::path& driver_manifest,
   return ADBC_STATUS_OK;
 }
 
-#if ADBC_CONDA_BUILD
+#ifdef ADBC_CONDA_BUILD
 #ifdef _WIN32
 std::filesystem::path GetEnvAsPath(const wchar_t* env_var) {
   size_t required_size;
@@ -311,7 +311,7 @@ std::vector<std::filesystem::path> GetSearchPaths(const AdbcLoadFlags levels) {
       paths = InternalAdbcParsePath(env_path);
     }
 
-#if ADBC_CONDA_BUILD
+#ifdef ADBC_CONDA_BUILD
 #ifdef _WIN32
     const wchar_t* conda_name = L"CONDA_PREFIX";
 #else
