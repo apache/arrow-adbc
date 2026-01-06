9693e1c0d45cf3b2b8c65713ad92ef34be8ad9f2
David Li <li.davidm96@gmail.com>
2026-01-06 12:58:03 +0900

    chore: update versions for 22

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ Apache, the Apache logo, and the Apache Arrow project logo are either
 registered trademarks or trademarks of The Apache Software Foundation in the
 United States and other countries."""
 author = "the Apache Arrow Developers"
-release = "22 (dev)"
+release = "22"
 # Needed to generate version switcher
 version = release
 
