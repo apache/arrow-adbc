apache/arrow-adbc

commit 886cfa53879ede4c3ad72ac97edfdf7ad6e7f1d8

fix(ci/scripts): correct csharp pack powershell script to set arguments correctly (#4304)
Corrects the command arguments to `dotnet pack` for the `--no-build`
flag.
- change the argument's value from $true to '' (empty string) as it is a
flag with no argument value.
- add a command to return the working folder back to the original it was
called from.

Co-authored-by: Bruce Irschick (Bit Quill Technologies Inc) <v-birschick@microsoft.com>

ci/scripts/csharp_pack.ps1
@@ -38,7 +38,11 @@ if ($versionSuffix) {
 }
 if ($noBuild) {
     Write-Host " * Pack without building"
-    $packArgs["-no-build"] = $true
+    # this argument is a flag and has no value, so we set it to an empty string,
+    # not $true or $false, which would be converted to "True" or "False"
+    $packArgs["-no-build"] = ""
 }
 
 dotnet pack @packArgs
+
+Set-Location -

commit b4f1e3e08c2413180fef4811e94d7677949da403

fix(ci/scripts): fix Set-Location syntax (#4307)
Correct bug introduced in PR
https://github.com/apache/arrow-adbc/pull/4304
- Instead uses the Push-Location / Pop-Location pair

ci/scripts/csharp_pack.ps1
@@ -22,7 +22,7 @@ param (
 
 $csharpFolder = [IO.Path]::Combine($PSScriptRoot, "..", "..", "csharp") | Resolve-Path
 Write-Host "Setting Path: $csharpFolder"
-Set-Location $csharpFolder
+Push-Location $csharpFolder
 
 Write-Host "Running dotnet pack -c Release"
 $packArgs = @{
@@ -45,4 +45,4 @@ if ($noBuild) {
 
 dotnet pack @packArgs
 
-Set-Location -
+Pop-Location